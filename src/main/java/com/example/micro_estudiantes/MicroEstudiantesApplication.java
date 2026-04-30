7d32cd70307a72522623988ecf96cf7d34bbcc76
Cristopher <cr.candiam@duouc.cl>
2026-04-29 21:55:17 -0400

    servicio crud y clase principal en la raiz del paquete

---
+++ src/main/java/com/example/micro_estudiantes/MicroEstudiantesApplication.java
@@ -0,0 +1,13 @@
+package com.example.micro_estudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroEstudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroEstudiantesApplication.class, args);
+	}
+
+}
